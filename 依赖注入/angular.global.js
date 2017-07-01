fb7b6fb1735a2d868c266f673c2a96691457b767
fanlina0522 <1439329228@qq.com>
2017-07-01 19:40:51 +0800

    angular

---
+++ 依赖注入/angular.global.js
@@ -0,0 +1,30 @@
+﻿var globalapp = angular.module('globalapp', []);
+//值
+globalapp.value('baseUrl', function () {
+    return "baseUrl";
+    //return {
+    //    calc: function (n1, n2) {
+    //        return n1 * 1 + n2 * 1;
+    //    }
+    //};
+})
+
+//工厂模式
+globalapp.factory('calcFactory', function () {
+    return {
+        calc: function (n1, n2) {
+            return n1 * 1 + n2 * 1;
+        }
+    };
+});
+//过滤器
+globalapp.filter('range', function () {
+    return function (array, range) {
+        //array => []
+        range = parseInt(range);
+        for (var i = 0; i < range; i++) {
+            array.push(i);
+        }
+        return array;
+    }
+})
---
+++ 路由/controllers.js
@@ -0,0 +1,14 @@
+﻿var mainApp = angular.module('mainApp', []);
+mainApp.controller('controller', function ($scope) {
+    $scope.name = 'pagetab';
+});
+mainApp.controller('controller1', ['$scope', '$stateParams', function ($scope, $stateParams) {
+    $scope.name = 'page1';
+    console.log($stateParams);
+}]);
+mainApp.controller('controller2', function ($scope) {
+    $scope.name = 'page2';
+});
+mainApp.controller('controller3', function ($scope) {
+    $scope.name = 'page3';
+});
